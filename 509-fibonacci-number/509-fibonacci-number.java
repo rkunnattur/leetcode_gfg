326527fad6a6414974f89abc47906ec04ca786e7
Radha Kunnattur <radzram@gmail.com>
2022-07-22 15:06:00 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (55.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,42 @@
 class Solution {
-    Map<Integer, Integer> cache;
-    
     public int fib(int n) {
-        this.cache = new HashMap<>(Map.of(0, 0, 1, 1));
-        return fibTopDownExtraSpace(n);
+        
+        //return fibTopDown(n);
+        return fibBottomUp(n);
     }
     
-    public int fibBottomUpExtraSpace(int n) {
-        if(n <= 1) return n;
+    public int fibTopDown(int n) {
+        // return fibRNoExSp(n);
+        int[] fib = new int[n+1];
+        Arrays.fill(fib, -1);
+        fib[0] = 0; fib[1] = 1;
         
+        return fibRExSp(n, fib);
+    }
+    
+    public int fibBottomUp(int n) {
+        
+        //return fibIExSp(n);
+        return fibINoExSp(n);
+    }
+    
+     public int fibINoExSp(int n) {
+         if(n <= 1) return n;
+         int prevprev = 0, prev = 1, curr = prev + prevprev;
+         
+         for(int i = 2; i <= n; i++) {
+             curr = prevprev + prev;
+             prevprev = prev;    
+             prev = curr;
+         }
+         
+         return curr;
+     }
+    
+    public int fibIExSp(int n) {
         int[] fib = new int[n+1];
         fib[1] = 1;
+        
         for(int i = 2; i <= n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
@@ -18,34 +44,16 @@ class Solution {
         return fib[n];
     }
     
-    public int fibBottomUpoExtraSpace(int n) {
+    public int fibRNoExSp(int n){
         if(n <= 1) return n;
         
-        int prev_2 = 0, prev_1 = 1, curr = 0;
-        for(int i = 2; i <= n; i++) {   
-            curr = prev_1 + prev_2;
-            prev_2 = prev_1;
-            prev_1 = curr;
-        }
-        
-        return curr;
+        return fibRNoExSp(n-1) + fibRNoExSp(n-2);
     }
     
-    
-    public int fibTopDownNoExtraSpace(int n) {
-        if(n <= 1) return n;
+    public int fibRExSp(int n, int[] fib) {
+        if(fib[n] != -1) return fib[n];
         
-        return fibTopDownNoExtraSpace(n-1) + fibTopDownNoExtraSpace(n-2);
+        fib[n] = fib[n-1] + fib[n-2];
+        return fib[n];
     }
-    
-    public int fibTopDownExtraSpace(int n) {
-        
-        
-        if(cache.containsKey(n)) return cache.get(n);
-        
-        cache.put(n, fibTopDownExtraSpace(n-1) + fibTopDownExtraSpace(n-2));
-        return cache.get(n);
-        
-    }
-    
 }
