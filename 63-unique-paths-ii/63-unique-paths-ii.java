2d777480457ca42a0302daaf03ae3b66667c3478
Radha Kunnattur <radzram@gmail.com>
2022-08-01 15:50:14 +0530

    Time: 1 ms (63.79%), Space: 42.2 MB (59.46%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         
-        return uniquePathWithObstaclesI(obstacleGrid);
+        return uniquePathsWithObstaclesIOpt1(obstacleGrid);
     }
     
     public int uniquePathWithObstaclesI(int[][] obstacleGrid) {
@@ -27,4 +27,28 @@ class Solution {
         
         return paths[rowLen-1][colLen-1];
     }
+    
+    // no extra space
+    public int uniquePathsWithObstaclesIOpt1(int[][] obstacleGrid) {
+            int rowLen = obstacleGrid.length;
+            int colLen = obstacleGrid[0].length;
+    
+            obstacleGrid[0][0] ^= 1;
+        
+            for(int r = 1; r<rowLen; r++){
+                obstacleGrid[r][0] = obstacleGrid[r][0] == 1 ? 0 : obstacleGrid[r-1][0];
+            }
+            
+            for(int c = 1; c<colLen; c++){
+                obstacleGrid[0][c] = obstacleGrid[0][c] == 1 ? 0 : obstacleGrid[0][c-1] ;
+            }
+        
+            for(int r = 1; r < rowLen; r++){
+                for(int c = 1; c < colLen; c++){
+                    obstacleGrid[r][c] = (obstacleGrid[r][c] == 1) ? 0 : obstacleGrid[r-1][c]+obstacleGrid[r][c-1];
+                }
+            }
+        
+            return obstacleGrid[rowLen-1][colLen-1];
+        }
 }
