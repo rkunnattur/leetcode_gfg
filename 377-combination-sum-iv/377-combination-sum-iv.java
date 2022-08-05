00bc0affeb04cb4a81f843464115885982217b3b
Radha Kunnattur <radzram@gmail.com>
2022-08-05 15:45:42 +0530

    Time: 2 ms (65.18%), Space: 42 MB (21.92%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -2,7 +2,8 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         
         
-        return combinationSum4R(nums, target, new Integer[target+1]);
+        //return combinationSum4R(nums, target, new Integer[target+1]);
+        return combinationSum4I(nums, target);
     }
     
     public int combinationSum4R(int[] nums, int target, Integer[] cs) {
@@ -19,4 +20,20 @@ class Solution {
         
         return cs[target] = count;
     }
+    
+    public int combinationSum4I(int[] nums, int target) {
+        
+        int[] cs = new int[target+1];
+        cs[0] = 1;
+        
+        for(int t = 1; t <= target; t++) {
+            for(int k: nums) {
+                if(t-k >= 0) cs[t] += cs[t - k];
+                else continue;
+            }
+        }
+        
+        return cs[target];
+    }
+    
 }
