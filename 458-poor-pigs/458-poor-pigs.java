779bc38bfe29bf608ca592c1cfecc838f72f64e2
Radha Kunnattur <radzram@gmail.com>
2022-08-06 20:38:20 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (5.60%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.java
+++ 458-poor-pigs/458-poor-pigs.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/poor-pigs/discuss/94273/Solution-with-detailed-explanation
+
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int states = (minutesToTest / minutesToDie) + 1;
