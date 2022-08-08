d62a8dc2145ff40f6a7574ac85257314b1fd1d29
Radha Kunnattur <radzram@gmail.com>
2022-08-08 14:50:44 +0530

    Time: 69 ms (73.58%), Space: 79.6 MB (70.84%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,33 +1,32 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        int n = envelopes.length;
-        if(n <= 1) return n;
-        Arrays.sort(envelopes, (o1, o2)->{
-            if(o1[0] != o2[0]) return o1[0] - o2[0];
-            else return o2[1] - o1[1];
+        int len = envelopes.length;
+        if(len <= 1) return len;
+        
+        Arrays.sort(envelopes, (o1, o2) -> {
+            return (o1[0] != o2[0]) ? o1[0] - o2[0] : o2[1] - o1[1];
         });
         
-        int[] heights = new int[n];
+        int[] height = new int[len];
         int idx = 0;
-        for(int[] envelope : envelopes){
-            heights[idx++] = envelope[1];
-        }
-        return lengthOfLIS(heights);
+        for(int[] envelope: envelopes) height[idx++] = envelope[1];
+    
+        return lis(height);
     }
     
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int len = 0;
+    public int lis(int[] nums) {
+        int len = nums.length;
+        int[] lis = new int[len];
         
-        for(int num : nums){
-            int i = Arrays.binarySearch(dp, 0, len, num);
-            if(i < 0){
-                i = - (i+1);
-            }
-            dp[i] = num;
-            if(i==len) len += 1;
-        }        
-        return len;        
-    }   
+        int end = 0;
+        for(int n: nums) {
+            int i = Arrays.binarySearch(lis, 0, end, n);
+            if(i < 0) i = -(i+1);
+            
+            lis[i] = n;
+            if(i == end) end++;
+        }
+        
+        return end;
+    }
 }
