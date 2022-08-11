0a96329d1e74d0d2fa90233ac1332cec887af882
Radha Kunnattur <radzram@gmail.com>
2022-08-11 13:27:05 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (72.83%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -17,7 +17,7 @@ class Solution {
     
     public int climbStairsI(int n, int[] ways) {
         if(n==1) return 1;
-        ways[0] = 1; ways[1] = 1; ways[2] = 2;
+        ways[0] = ways[1] = 1; ways[2] = 2;
         
         for(int i = 3; i <= n; i++) ways[i] = ways[i-1] + ways[i-2];
         
