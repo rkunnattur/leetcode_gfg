02cbc25fbb62963c15c3703dd57b248a19e62d95
Radha Kunnattur <radzram@gmail.com>
2022-08-15 22:33:00 +0530

    Time: 9 ms (58.60%), Space: 46.8 MB (29.88%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -4,8 +4,9 @@ class Solution {
        Map<Character, Integer> numericalRepresentation = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
         
         int sum = 0, prev=0, temp = 0;
-        for(int i = s.length()-1; i >=0; i--) {
+        for(int i = s.length()-1; i >=0; i--) { // iterate ffrom last, if prev number is smaller than the subsequent number, deduct from sum, else add to sum.
             temp = numericalRepresentation.get(s.charAt(i));
+            // VI is a 6 -> 5 + 1 = 6, but the reverse is not same, IV -> 5 -1 = 4
             sum += (temp < prev) ? (-1 * temp) : temp;
             prev = temp;
         }
