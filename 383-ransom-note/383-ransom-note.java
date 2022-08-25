d1bee2f535278b41f176eef918e041673fe099e5
Radha Kunnattur <radzram@gmail.com>
2022-08-25 06:14:08 +0530

    Time: 4 ms (84.52%), Space: 46.4 MB (58.29%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[][] countChar = new int[26][26];
+        int[] countChar = new int[26];
         
         for(char ch: magazine.toCharArray()) {
-            countChar[0][ch-'a']++;
+            countChar[ch-'a']++;
         }
         
         for(char ch: ransomNote.toCharArray()) {
-            countChar[1][ch-'a']++;
-        }
-        
-        for(int i = 0; i < 26; i++) {
-            if(countChar[1][i] > 0 && countChar[1][i] > countChar[0][i]) return false;
+            if(--countChar[ch-'a'] < 0) return false;
         }
         
         return true;
