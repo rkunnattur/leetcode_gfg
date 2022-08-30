385278b0b1c365b708078b50f3d536f7510d1776
Radha Kunnattur <radzram@gmail.com>
2022-08-30 15:29:27 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (93.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,8 +2,8 @@ class Solution {
     public void rotate(int[][] matrix) {
          int n = matrix.length; // since its n*n matrix  we dont need to consider column as such both are same
 
-         for (int i = 0; i< ((n+1)>>1); i++) {
-             for (int j = 0; j< (n>>1); j++) {
+         for (int i = 0; i < n; i++) {
+             for (int j = i; j < n-1-i; j++) {
                  int temp = matrix[i][j];
                  matrix[i] [j] = matrix[n-1-j][i];
                  matrix[n-1-j] [i] = matrix[n-1-i][n-1-j];
