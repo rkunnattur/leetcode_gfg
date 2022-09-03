0a2a162083047bdc5c318d2bd4f5835b90bc3eef
Radha Kunnattur <radzram@gmail.com>
2022-09-03 12:38:00 +0530

    Time: 1 ms (98.91%), Space: 41.8 MB (89.42%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -16,7 +16,7 @@ public class Solution {
         int res = 0;
         for(int i=0;i<32;i++){
             int bit = (n >> i) & 1;
-            res = res + (bit<<(31-i));
+            res |= (bit<<(31-i));
         }
         return res;
     }
