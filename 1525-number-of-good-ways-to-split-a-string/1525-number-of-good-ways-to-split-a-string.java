f3607adeabd06276011bae28463aedf1db19850a
Radha Kunnattur <radzram@gmail.com>
2022-09-05 05:41:40 +0530

    Time: 42 ms (38.19%), Space: 46.2 MB (68.51%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int numSplits(String s) {
+    
+    
+    /* Approach1: sliding window technique, with left n right preffixx arrays
+        */
+    
+    public int numSplits1(String s) {
         int slen = s.length(), rightCount = 0;
         int[] prefixRightCount = new int[s.length()];
         
@@ -20,4 +25,30 @@ class Solution {
         
         return goodSplitCount;
     }
+    
+    //https://leetcode.com/problems/number-of-good-ways-to-split-a-string/discuss/2340113/.Explanation-and-ways-of-approaching-similar-kind-of-problems
+    
+    /*
+        Approach2: slidingWindow Technique, withh preffix Array n HashSet
+    */
+     public int numSplits(String s) {
+         int len = s.length();
+         
+         int[] prefix = new int[len];
+         Set<Character> distinctChar = new HashSet<>();
+         for(int i = len-1; i >= 0; i--) {
+             prefix[i] = distinctChar.size();   
+             distinctChar.add(s.charAt(i)); // count of distinct char
+         }
+         
+         distinctChar.clear();
+         int goodSplit = 0;
+         for(int i = 0; i < len; i++) {
+             distinctChar.add(s.charAt(i));
+             if(distinctChar.size() == prefix[i]) goodSplit++;
+         }
+         
+         return goodSplit;
+     }
+    
 }
