17058139f851e6a1624cea5bfd3292dc75890f83
Radha Kunnattur <radzram@gmail.com>
2022-09-08 05:43:16 +0530

    Time: 5 ms (95.75%), Space: 56.1 MB (28.41%) - LeetHub

---
+++ 89-gray-code/89-gray-code.java
@@ -0,0 +1,20 @@
+class Solution {
+    int num = 0;
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        computeGrayCode(res, n);
+        return res;
+    }
+    
+    public void computeGrayCode(List<Integer> res, int n){
+        if(n==0) {
+            res.add(num);
+            return;
+        }
+        
+        computeGrayCode(res, n-1);
+        num = num^(1<<(n-1));
+        computeGrayCode(res, n-1);
+
+    }
+}
